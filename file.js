6f94416074fb15b416ce7846882b1b5f8ec54e67
shreyashnand <shreyash.nandgaonkar7@gmail.com>
2025-04-16 20:46:07 +0530

    Feat: Add support for custom delimiters

--- file.js
+++ file.js
@@ -1,7 +1,22 @@
 function add(numbers) {
     if (!numbers) return 0;
 
-    const numberList = numbers.split(/,|\n/).map(n => parseInt(n, 10));
+    let delimiterPattern = /,|\n/;
+    let numberString = numbers;
+    
+    if (numbers.startsWith("//")) {
+        const delimiterEndIndex = numbers.indexOf("\n");
+        const customDelimiter = numbers.slice(2, delimiterEndIndex);
+
+        // Escape special regex characters in delimiter
+        const escapedDelimiter = customDelimiter.replace(/[[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+        delimiterPattern = new RegExp(escapedDelimiter);
+
+        // Get the actual numbers part of the string
+        numberString = numbers.slice(delimiterEndIndex + 1);
+    }
+
+    const numberList = numberString.split(delimiterPattern).map(n => parseInt(n, 10));
 
     const sum = numberList.reduce((total, num) => total + num, 0);
 
@@ -14,3 +29,5 @@ console.log(add("1"));     // Output: 1
 console.log(add("1,5"));  // Output: 6
 console.log(add("1\n2,3"));  // Output: 6
 console.log(add("4\n5\n6")); // Output: 15
+console.log(add("//;\n1;2"));   // 3
+console.log(add("//|\n4|5|6")); // 15
