a33edc69598a2dec55511ae5cc99993c4480413e
shreyashnand <shreyash.nandgaonkar7@gmail.com>
2025-04-16 20:41:57 +0530

    Feat: Handle newlines as valid delimeters

--- file.js
+++ file.js
@@ -1,7 +1,7 @@
 function add(numbers) {
     if (!numbers) return 0;
 
-    const numberList = numbers.split(',').map(n => parseInt(n, 10));
+    const numberList = numbers.split(/,|\n/).map(n => parseInt(n, 10));
 
     const sum = numberList.reduce((total, num) => total + num, 0);
 
@@ -12,3 +12,5 @@ function add(numbers) {
 console.log(add(""));      // Output: 0
 console.log(add("1"));     // Output: 1
 console.log(add("1,5"));  // Output: 6
+console.log(add("1\n2,3"));  // Output: 6
+console.log(add("4\n5\n6")); // Output: 15
