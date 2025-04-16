e34f1680243d45a78eea9aa88dc618f143fe878f
shreyashnand <shreyash.nandgaonkar7@gmail.com>
2025-04-16 20:26:28 +0530

    Initial commit: Adds one or two number

---
+++ file.js
@@ -0,0 +1,14 @@
+function add(numbers) {
+    if (!numbers) return 0;
+
+    const numberList = numbers.split(',').map(n => parseInt(n, 10));
+
+    const sum = numberList.reduce((total, num) => total + num, 0);
+
+    return sum;
+}
+
+// Cases
+console.log(add(""));      // Output: 0
+console.log(add("1"));     // Output: 1
+console.log(add("1,5"));  // Output: 6
